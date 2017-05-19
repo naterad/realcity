13fc45b5cd6b8d579c1d976b02b73f85b761f80c
Nathan Radmall <nathanradmall@gmail.com>
2017-05-19 15:59:43 -0600

    restructure

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// function active(){
+//   var searchBar = document.getElemenentById('search');
+//
+//   if (searchBar.value == "San Antonio, TX") {
+//     searchBar.value = ""
+//     searchBar.placeholder = 'San Antonio, TX'
+//     searchBar.placeholder.color = black;
+//   }
+// }
+// function inactive(){
+//   var searchBar = document.getElemenentById('search');
+//
+//   if (searchBar.value == "") {
+//     searchBar.value = 'San Antonio, TX'
+//     searchBar.placeholder = ''
+//   }
+// }
+
+
+function loadHome(){
+  console.log("loadHome");
+}
+function submitSearch(){
+  console.log($("#search_input").val());
+}
+function autoComplete(){
+  // this is where we will add the auto complete
+  // console.log($("#search_input").val());
+}
